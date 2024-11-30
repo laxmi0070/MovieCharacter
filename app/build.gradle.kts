3c28bfbce023363515487a3c99955112f35cc703
laxmi0070 <laxmi.sjce@gmail.com>
2024-11-30 13:35:40 +0530

    Added Coil Dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -93,4 +93,7 @@ dependencies {
     implementation(libs.androidx.lifecycle.viewmodel.compose)
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.hilt.navigation.compose)
+
+    // Coil
+    implementation(libs.coil.compose)
 }
